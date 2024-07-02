fa31d17b5f04cd5f9d3cc58142d2749a4bf0ebf2
adelaide <adelaide.chanteux@epitech.eu>
2024-07-02 15:56:12 +0100

    Add basic google auth

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    </div>
+    <div>
+        <slot />
+    </div>
+
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '../store/auth';
+
+const router = useRouter();
+
+const { logUserOut } = useAuthStore();
+const { authenticated } = storeToRefs(useAuthStore());
+
+const logout = () => {
+  logUserOut();
+  router.push('/login');
+};
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    'nuxt-vue3-google-signin'
+  ],
+  googleSignIn: {
+    clientId: "80772791160-i0v9t46ic6cuqj38u6g5gcs0kh7c6opk.apps.googleusercontent.com",
+  }
 })
