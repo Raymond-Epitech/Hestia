07da2a6e694b748632bf10cb6519020340e45d28
thibault.mir@epitech.eu <thibault.mir@epitech.eu>
2024-07-02 11:17:39 +0200

    Init backend

---
+++ backend/Api/Configuration/ServiceConfigurationExtension.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreApi.Services;
+using Business.Interfaces;
+
+namespace WebApi.Configuration
+{
+    public static class ServiceConfigurationExtension
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration, bool isDevelopment)
+        {
+            services.AddDbContext(configuration, isDevelopment)
+                .AddBusinessServices()
+                .EnableCors();
+        }
+
+        private static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITestService, TestService>();
+            services.AddSingleton<TestService>();
+            services.AddHttpContextAccessor();
+            return services;
+        }
+
+        public static IServiceCollection EnableCors(this IServiceCollection services)
+        {
+            var allowedHost = new[]{
+                "http://localhost:4200"
+            };
+            services.AddCors(opt => opt.AddDefaultPolicy(policy => policy
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowed(host => allowedHost.Contains(host))));
+            return services;
+        }
+
+        private static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration, bool isDevelopment)
+        {
+            //DB context
+
+            return services;
+        }
+    }
+}
---
+++ backend/Api/Program.cs
@@ -0,0 +1,74 @@
+using BookStoreApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApi.Configuration;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseDefaultServiceProvider(o =>
+    {
+        o.ValidateOnBuild = true;
+        o.ValidateScopes = true;
+    });
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.ConfigureServices(builder.Configuration, builder.Environment.IsDevelopment());
+
+    builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+    builder.Services.AddAuthorization();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseCors();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+    return 1;
+}
+
+return 0;
