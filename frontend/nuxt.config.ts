bd84fe22c6ffe15fa722a6d2ef9ff85494093a09
adelaide <adelaide.chanteux@epitech.eu>
2024-09-01 10:45:14 +0100

    Add navbar and placeholders

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,6 +7,12 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'nuxt-vue3-google-signin'
   ],
+  css: [
+    '~/assets/styles/main.scss',
+  ],
+  plugins: [
+    { src: '~/plugins/bootstrap.js', mode: 'client' },
+  ],
   googleSignIn: {
     clientId: "80772791160-i0v9t46ic6cuqj38u6g5gcs0kh7c6opk.apps.googleusercontent.com",
   }
