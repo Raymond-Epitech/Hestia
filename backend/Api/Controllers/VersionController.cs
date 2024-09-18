6230071925f12a6c9caff9a611879a25e82f8c5b
thibault.mir@epitech.eu <thibault.mir@epitech.eu>
2024-09-18 17:15:08 +0900

    Add version controller for testing

---
+++ backend/Api/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreApi.Services;
+using Business.Exceptions;
+using Business.Models.Output;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetVersion()
+        {
+            return Ok("1.0");
+        }
+    }
+}
