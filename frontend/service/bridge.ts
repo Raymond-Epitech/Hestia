e329ae4238ecdacaef31d952932b3f179a799e26
Benjamin <benjamin.bourez@epitech.eu>
2024-10-11 12:20:36 +0900

    Add all reminder route for bridge and get version

--- frontend/service/bridge.ts
+++ frontend/service/bridge.ts
@@ -1,3 +1,5 @@
+import type { Reminder } from "./type";
+
 class bridge {
     url: string = "http://localhost:8080";
 
@@ -5,9 +7,17 @@ class bridge {
         this.url = new_url;
     }
 
-    async getTest() {
-        return await fetch(this.url + "/Test").then(response => response.json());
+    // Version section: get version
+
+    async getVersion() {
+        const response: Response = await fetch(this.url + "/api/Version");
+        if (response.status == 200) {
+            return await response.json();
+        }
+        return {};
     }
+
+    // Reminder section: get all reminders, get reminder by ID, add reminder, update reminder, delete reminder
 
     async getAllReminders() {
         const response: Response = await fetch(this.url + "/api/Reminder");
@@ -25,13 +35,29 @@ class bridge {
         return {};
     }
 
-    async addReminder(data: any) {
+    async addReminder(data: Reminder) {
         return await fetch(this.url + "/api/Reminder", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
+        }).then(response => response.json());
+    }
+
+    async updateReminder(data: Reminder) {
+        return await fetch(this.url + "/api/Reminder/" + data.id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json());
+    }
+
+    async deleteReminder(id: string) {
+        return await fetch(this.url + "/api/Reminder/" + id, {
+            method: 'DELETE'
         }).then(response => response.json());
     }
 }
