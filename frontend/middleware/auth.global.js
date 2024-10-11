b2025d42d5c956abb3e0885358da6c3fb0d692f1
adelaide <adelaide.chanteux@epitech.eu>
2024-10-11 09:16:37 +0100

    Fix login

--- frontend/middleware/auth.global.js
+++ frontend/middleware/auth.global.js
@@ -9,12 +9,12 @@ export default defineNuxtRouteMiddleware((to) => {
     authenticated.value = true;
   }
 
-  // if (token.value && to?.name === 'login') {
-  //   return navigateTo('/');
-  // }
+  if (token.value && to?.name === 'login') {
+    return navigateTo('/');
+  }
 
-  // if (!token.value && to?.name !== 'login') {
-  //   abortNavigation();
-  //   return navigateTo('/login');
-  // }
+  if (!token.value && to?.name !== 'login') {
+    abortNavigation();
+    return navigateTo('/login');
+  }
 });
