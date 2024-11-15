08e289c2822f9cd7e5b09d8d6f1d2bc47c91b548
Benjamin <benjamin.bourez@epitech.eu>
2024-11-15 15:32:51 +0900

    Add skeleton of the page

---
+++ frontend/pages/settings.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="conteneur">
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('langue')" id="langue">   
+            <h1> Language </h1>
+        </Rectangle>
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('notif')"  id="notif">
+            <h1> Notifications </h1>
+        </Rectangle>
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('acces')" id="acces">
+            <h1> Accessibilité</h1>
+        </Rectangle>
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('regle')" id="regle">
+            <h1> Regles de vie</h1>
+        </Rectangle>
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('conf')" id="conf">
+            <h1> Confidentialitée</h1>
+        </Rectangle>
+        <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('info')" id="info">
+            <h1> Informations</h1>
+        </Rectangle>
+    </div>
+</template>
+
+<script setup>
+const handleComposantClick = (id) => {
+  console.log(`ID du composant cliqué: ${id}`);
+  alert(`ID du composant cliqué: ${id}`);
+}
+</script>
+
+<style scoped>
+.conteneur {
+    display: grid;
+    gap: 40px;
+    width: 80%;
+    margin: 40px auto;
+}
+</style>
