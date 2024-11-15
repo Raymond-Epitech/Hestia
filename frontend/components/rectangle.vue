6393a977eed9cf77dadaf90ac324296174927260
Benjamin <benjamin.bourez@epitech.eu>
2024-11-15 14:52:03 +0900

    Add rectangle background

---
+++ frontend/components/rectangle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="rectangle" :style="{ backgroundColor: color }">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Rectangle",
+    props: {
+        color: {
+            type: String,
+            default: "white"
+        }
+    }
+};
+</script>
+
+<style scoped>
+.rectangle {
+    padding: 10px;
+    border-radius: 20px;
+}
+</style>
