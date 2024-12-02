3b6fafbce478385092807aceff0932a811fbae6e
Benjamin <benjamin.bourez@epitech.eu>
2024-12-02 16:54:48 +0900

    Add internationalization support with language selection and translations for language

--- frontend/pages/settings/language.vue
+++ frontend/pages/settings/language.vue
@@ -1,47 +1,47 @@
 <template>
   <NuxtPage />
   <div class="container">
-        <Rectangle color="#85AD7B" id="fr">
+        <Rectangle color="#85AD7B" id="fr" @click="setLocale('fr')">
           <div class="inner-class">
-            <h1> Français </h1>
             <img src="/flags/france.png" alt="France" width="64" height="64"/>
+            <h1> {{ $t('French') }} </h1>
           </div>
         </Rectangle>
-        <Rectangle color="#85AD7B" id="ang">
+        <Rectangle color="#85AD7B" id="ang" @click="setLocale('en')">
           <div class="inner-class">
-              <h1> Anglais </h1>
-              <img src="/flags/united-kingdom.png" alt="United Kingdom" width="64" height="64"/>
+            <img src="/flags/united-kingdom.png" alt="United Kingdom" width="64" height="64"/>
+              <h1> {{ $t('English') }} </h1>
             </div>
         </Rectangle>
-        <Rectangle color="#85AD7B" id="es">
+        <Rectangle color="#85AD7B" id="es" @click="setLocale('es')">
           <div class="inner-class">
-            <h1> Espagnol</h1>
             <img src="/flags/spain.png" alt="Spain" width="64" height="64"/>
+            <h1> {{ $t('Spanish') }} </h1>
           </div>
         </Rectangle>
-        <Rectangle color="#85AD7B" id="allemand">
+        <Rectangle color="#85AD7B" id="allemand" @click="setLocale('de')">
           <div class="inner-class">
-            <h1> Allemand</h1>
             <img src="/flags/germany.png" alt="Germany" width="64" height="64"/>
+            <h1> {{ $t('German') }} </h1>
           </div>
         </Rectangle>
-        <Rectangle color="#85AD7B" id="mandarin">
+        <Rectangle color="#85AD7B" id="mandarin" @click="setLocale('zh')">
           <div class="inner-class">
-            <h1> Mandarin</h1>
             <img src="/flags/china.png" alt="China" width="64" height="64"/>
+            <h1> {{ $t('Chinese') }} </h1>
           </div>
         </Rectangle>
-        <Rectangle color="#85AD7B" id="japonais">
+        <Rectangle color="#85AD7B" id="japonais" @click="setLocale('ja')">
           <div class="inner-class">
-            <h1> Japonais</h1>
             <img src="/flags/japan.png" alt="Japan" width="64" height="64"/>
+            <h1> {{ $t('Japanese') }} </h1>
           </div>
         </Rectangle>
     </div>
 </template>
 
 <script setup lang="ts">
-
+const { setLocale } = useI18n();
 </script>
 
 <style scoped>
