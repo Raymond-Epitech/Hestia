554fdf8cb3ae471b9cdddeea43343b5bfb74c66b
Benjamin <benjamin.bourez@epitech.eu>
2024-12-09 16:16:36 +0900

    ADD texte_language component to auto switch texte with language

---
+++ frontend/components/texte_language.vue
@@ -0,0 +1,14 @@
+<template>
+{{ $t(source) }} 
+</template>
+
+<script>
+export default {
+    props: {
+        source: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
