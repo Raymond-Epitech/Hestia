cd0d1a3f1e85a277a2298edd298be9a401528c44
Benjamin <benjamin.bourez@epitech.eu>
2025-02-07 16:02:13 +0700

    ADD translate index settings

--- frontend/pages/settings/index.vue
+++ frontend/pages/settings/index.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="conteneur">
         <Rectangle color="#85AD7B" :onClick="() => redirect('/settings/language')" id="language">
-            <h1> Language </h1>
+            <h1> <Texte_language source="Language" /> </h1>
         </Rectangle>
         <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('notif')" id="notif">
-            <h1> Notifications </h1>
+            <h1> <Texte_language source="Notifications" /> </h1>
         </Rectangle>
         <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('acces')" id="acces">
-            <h1> Accessibilité</h1>
+            <h1> <Texte_language source="Accessibility" /> </h1>
         </Rectangle>
         <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('regle')" id="regle">
-            <h1> Regles de vie</h1>
+            <h1> <Texte_language source="Rules" /> </h1>
         </Rectangle>
         <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('conf')" id="conf">
-            <h1> Confidentialitée</h1>
+            <h1> <Texte_language source="Confidentiality" /> </h1>
         </Rectangle>
         <Rectangle color="#85AD7B" :onClick="() => handleComposantClick('info')" id="info">
-            <h1> Informations</h1>
+            <h1> <Texte_language source="Informations" /> </h1>
         </Rectangle>
     </div>
 </template>
