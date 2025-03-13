6714599b4a2e44ebabc34d21febc2a06f8b17e9c
Benjamin <benjamin.bourez@epitech.eu>
2025-03-13 11:39:13 +0900

    UPDATE money page layout and add reactive global variable for financial management

--- frontend/pages/money.vue
+++ frontend/pages/money.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Hello user, you can manage your money here</h1>
+    <Rectangle color="#4FA3A6" id="rec_gdepense"><Texte_language source="global"/> {{ global }} €</Rectangle>
+    <Rectangle color="#FFF973" id="rec_regularize"><Texte_language source="regularize"/></Rectangle>
 </template>
+
+<script setup>
+const global = ref(0);
+</script>
