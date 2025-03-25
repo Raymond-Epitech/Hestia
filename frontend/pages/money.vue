b5e3047b87142d9c04d2bb9d1c2e0e486a9ec17c
Benjamin <benjamin.bourez@epitech.eu>
2025-03-25 17:05:05 +0900

    REFACTO money page layout by separating components and enhancing navigation functionality

--- frontend/pages/money.vue
+++ frontend/pages/money.vue
@@ -1,68 +1,10 @@
 <template>
-    <div  class="center-container">
-        <Rectangle color="#85AD7B" id="rec" class="center mini_rec">
-            <Texte_language source="food_expenses" /> 
-            <p class="regularize-text">
-                {{ food }} €
-            </p>
-        </Rectangle>
-        <Rectangle color="#85AD7B" id="rec" class="center mini_rec">
-            <Texte_language source="Health_expenses" /> 
-            <p class="regularize-text">
-                {{ health }} €
-            </p>
-        </Rectangle>
-        <Rectangle color="#85AD7B" id="rec" class="center mini_rec">
-            <Texte_language source="partie_expenses" /> 
-            <p class="regularize-text">
-                {{ partie }} €
-            </p>
-        </Rectangle>
-        <Rectangle color="#4FA3A6" id="rec" class="center mini_rec">
-            <Texte_language source="global" />
-            <p class="regularize-text">
-                {{ global }} €
-            </p>
-        </Rectangle>
-        <Rectangle color="#FFF973" id="rec" class="center regularize-text mini_rec">
-            <Texte_language source="regularize" />
-        </Rectangle>
+    <div>
+        <NuxtPage />
     </div>
 </template>
 
 <script setup>
-const global = ref(0);
-const food = ref(0);
-const health = ref(0);
-const partie = ref(0);
 </script>
 
-<style scoped>
-.center-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    text-align: center;
-}
-
-.mini_rec {
-    width: 50%;
-    margin: 10px;
-    padding: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-}
-
-.center {
-    text-align: center;
-}
-
-.regularize-text {
-    font-size: 24px;
-    font-weight: bold;
-}
-</style>
+<style scoped></style>
