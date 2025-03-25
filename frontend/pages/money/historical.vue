43ed63bc83fde418743be36b9ebe52a73cb3b2b9
Benjamin <benjamin.bourez@epitech.eu>
2025-03-25 17:05:16 +0900

    ADD historical page component with basic layout and navigation

---
+++ frontend/pages/money/historical.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="background">
+        <img src="/return.png" alt="Return" width="30" height="30" @click="$router.back()"/>
+        <div>
+            l'historique de {{ name }} arrive bientôt
+        </div>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const name = route.query.name; 
+</script>
+
+<style scoped>
+.background {
+    height: 100vh;
+    background-color: #1E1E1E;
+    color: white;
+    padding: 20px;
+}
+</style>
