bf175a4eca495d278c171f78d6971345db71f93f
Benjamin <benjamin.bourez@epitech.eu>
2025-03-27 15:51:15 +0900

    ADD ExpenseItem component for displaying individual expense details

---
+++ frontend/components/ExpenseItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <AddExpenseModal v-model="isModalOpen"/>
+    <div class="expense">
+      <div class="expense-header">
+        <span class="expense-name">{{ expense.name }}</span>
+        <span class="expense-amount">{{ expense.amount }} €</span>
+      </div>
+      <div class="payer">
+        <span>Payé par {{ expense.paidBy }}</span>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    expense: Object
+  });
+  </script>
+  
+  <style scoped>
+  .expense {
+    border-bottom: 2px solid #ddd;
+    padding: 10px 0;
+  }
+  
+  .expense-header {
+    display: flex;
+    justify-content: space-between;
+    font-weight: bold;
+  }
+  
+  .payer {
+    color: rgb(214, 207, 207);
+  }
+  </style>
