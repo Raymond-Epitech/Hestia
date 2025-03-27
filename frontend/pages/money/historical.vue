55d9ae6ac9b38509ab290f42c5fe1230c69117d3
Benjamin <benjamin.bourez@epitech.eu>
2025-03-27 15:51:34 +0900

    ADD historical page layout with header and expense item display

--- frontend/pages/money/historical.vue
+++ frontend/pages/money/historical.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="background">
-        <img src="/return.png" alt="Return" width="30" height="30" @click="$router.back()"/>
+        <div class="header">
+            <img src="/return.png" alt="Return" width="30" height="30" @click="$router.back()"/>
+            <div class="square">
+                <Rectangle color="#FFF973" id="add" >
+                    <img src="/plus.png" alt="Return" width="30" height="30" />
+                </Rectangle>
+            </div>
+        </div>
         <div>
-            l'historique de {{ name }} arrive bientôt
+            <ExpenseItem v-for="expense in expenses_list" :key="expense.id" :expense="expense" />
         </div>
     </div>
 </template>
@@ -10,6 +17,15 @@
 <script setup>
 const route = useRoute();
 const name = route.query.name; 
+const expenses_list = ref([]);
+//en attendant de pouvoir récupérer les données
+expenses_list.value = [
+    {"name": "food", "amount": 100, "paidBy": "Tibo"},
+    {"name": "sucrerie", "amount": 200, "paidBy": "sugarDaddy"},
+    {"name": "miamiam", "amount": 300, "paidBy": "Benji"},
+    {"name": "viande", "amount": 600, "paidBy": "Adé"},
+    {"name": "bon shit sa mère", "amount": 420.50, "paidBy": "Raimon"}
+];
 </script>
 
 <style scoped>
@@ -18,5 +34,17 @@ const name = route.query.name;
     background-color: #1E1E1E;
     color: white;
     padding: 20px;
+}
+
+.square {
+    width: 50px;
+    height: 64px;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
 }
 </style>
