c476cb3d557c30efe8ec4602e917990c42bc9944
adelaide chanteux <adelaide.chanteux@smobe.fr>
2025-04-09 13:38:06 +0200

    Add android base build

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.volume.hestia',
-    appName: 'Hestia',
-    webDir: 'dist',
+  appId: 'io.ionic.starter',
+  appName: 'Hestia',
+  webDir: 'dist'
 };
 
 export default config;
