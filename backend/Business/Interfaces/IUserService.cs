dfe4955fd5d4f4fedc5e163538534d7987893ee5
thibault.mir@epitech.eu <thibault.mir@epitech.eu>
2025-04-23 20:04:31 +0900

    Add google code verification into the login and register route

--- backend/Business/Interfaces/IUserService.cs
+++ backend/Business/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Jwt;
+using Shared.Models.DTO;
 using Shared.Models.Input;
 using Shared.Models.Output;
 using Shared.Models.Update;
@@ -12,7 +13,7 @@ namespace Business.Interfaces
         Task<Guid> UpdateUserAsync(UserUpdate user);
         Task<Guid> DeleteUserAsync(Guid id);
         Task<Guid> QuitColocationAsync(Guid id);
-        Task<UserInfo> RegisterUserAsync(string googleToken, UserInput userInput);
-        Task<UserInfo> LoginUserAsync(string googleToken);
+        Task<UserInfo> RegisterUserAsync(string code, UserInput userInput, GoogleCredentials googleCredentials);
+        Task<UserInfo> LoginUserAsync(string code, GoogleCredentials googleCredentials);
     }
 }
