e6a1bb85a1845abfd9d8baff7fea205490a46ad4
adelaide chanteux <adelaide.chanteux@smobe.fr>
2025-05-14 18:36:47 +0200

    Add generated files

---
+++ dist/_nuxt/B0-eC6DN.js
@@ -0,0 +1,1 @@
+import{_ as e,a as r,b as s}from"./342sUR_Q.js";const t={};function a(c,o){return s(),r("h1",null," Hello user, here are your tasks")}const _=e(t,[["render",a]]);export{_ as default};
