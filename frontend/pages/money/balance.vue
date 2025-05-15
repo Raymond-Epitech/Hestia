4f6c253edfca194b1673ecf7810a0ca20facc547
Benjamin <benjamin.bourez@epitech.eu>
2025-05-15 16:53:23 +0900

    ADD new page for refund

--- frontend/pages/money/balance.vue
+++ frontend/pages/money/balance.vue
@@ -16,7 +16,7 @@
                 </h4>
             </Rectangle>
             <rectangle color="#4FA3A6" id="rec" class="center regularize-text mini_rec"
-                :onClick="() => redirectto('balance')">
+                :onClick="() => redirectto('refund')">
                 <Texte_language source="refund" />
             </rectangle>
         </div>
@@ -36,6 +36,7 @@ const api = $bridge;
 api.setjwt(useCookie('token').value ?? '');
 const collocid = user.colocationId
 const myid = user.id
+const router = useRouter();
 
 api.getBalance(collocid).then((response) => {
     list_balance.value = response;
@@ -51,6 +52,12 @@ api.getUserbyCollocId(collocid).then((response) => {
 const getBalance = (id: string): number => {
     return list_balance.value?.[id] ?? 0;
 };
+const redirectto = (name: string) => {
+    if (name === 'refund') {
+        router.push({ path: '/money/refund', query: { list_coloc: JSON.stringify(list_coloc.value) }  });
+        return;
+    }
+}
 </script>
 
 
