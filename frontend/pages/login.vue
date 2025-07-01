d127001c7086ed516ce8989b37653e67ce1883f5
adelaide chanteux <adelaide.chanteux@smobe.fr>
2025-07-01 19:36:54 +0200

    Fix login redirection button when on register

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -10,7 +10,7 @@
             <input class="input" type="text" placeholder="Optionel" v-model="colocationID" />
             <h2 class="register-font">Créer un compte :</h2>
             <a type="submit" @click.prevent="register()" class="google-button"> Register with Google</a>
-            <button class="register-button" @click="login()">Login</button>
+            <button class="register-button" @click="goLogin()">Login</button>
         </div>
         <div v-else class="login">
             <h2 class="login-font">Login : </h2>
@@ -50,6 +50,10 @@ onMounted(() => {
         }
     })
 })
+
+function goLogin() {
+    registretion.value = false;
+}
 
 function goRegister() {
     registretion.value = true;
