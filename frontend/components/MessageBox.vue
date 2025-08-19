6665c7d1422cd9ad9e90f6a2d089ac9b7b27abdd
Benjamin <benjamin.bourez@epitech.eu>
2025-08-19 19:01:22 +0200

    Add message display and sending functionality with MessageBox component

---
+++ frontend/components/MessageBox.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="box">
+        <div v-if="sendBy != 'me'" class="header">
+            <ProfileIcon :height="33" :width="33"/>
+            <p>{{ sendBy }}</p>
+        </div>
+        <div :class="['content', sendBy === 'me' ? 'content-me' : 'content-other']">
+            <p>{{ content }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    content: {
+        type: String,
+        required: true
+    },
+    sendBy: {
+        type: String,
+        default: ''
+    },
+
+
+})
+</script>
+
+<style scoped>
+.box {
+    color: #4B4B4B;
+    padding: 10px;
+    border-radius: 8px;
+    margin-bottom: 10px;
+}
+.header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+}
+
+.header p {
+    margin-left: 10px;
+    font-weight: bold;
+}
+
+.content {
+    color: black;
+    align-content: center;
+}
+
+.content-me {
+    font-size: 14px;
+    border-radius: 8px 8px 0 8px;
+    background-color: #BDD4F6
+}
+.content-other {
+    font-size: 14px;
+    border-radius: 8px 8px 8px 0;
+    background-color: #C1E0B9
+}
+</style>
--- frontend/pages/message.vue
+++ frontend/pages/message.vue
@@ -1,13 +1,72 @@
 <template>
-    
+    <div class="background">
+        <div v-for="msg in messages" :key="msg.id">
+            <MessageBox :content="msg.content" :sendBy="getUsername(msg.sendBy)" />
+        </div>
+        <form @submit.prevent="handleSendMessage">
+            <input v-model="newMessage.content" type="text" placeholder="Message" class="body-input"
+                required />
+            <button type="submit" class="button">
+                <img src="/Submit.svg" alt="Submit Icon" class="svg-icon" />
+            </button>
+        </form>
+    </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useUserStore } from '~/store/user';
+import type { Coloc, message } from '~/composables/service/type';
+
 const userStore = useUserStore();
 const { $bridge } = useNuxtApp()
 const api = $bridge;
 api.setjwt(useCookie('token').value ?? '');
+const colocationId = userStore.user.colocationId;
+const messages = ref<message[]>([]);
+const list_coloc = ref<Coloc[]>([]);
+
+const fetchMessages = async () => {
+    try {
+        messages.value = await api.getMessageByColocationId(colocationId);
+    } catch (error) {
+        console.error('Error fetching messages:', error);
+    }
+};
+
+onMounted(() => {
+    fetchMessages();
+    api.getUserbyCollocId(colocationId)
+        .then((colocs: Coloc[]) => {
+            list_coloc.value = colocs;
+        });
+});
+const newMessage = ref<message>({
+    colocationId: userStore.user.colocationId,
+    content: '',
+    sendBy: userStore.user.id,
+});
+const handleSendMessage = async () => {
+    try {
+        await api.addMessage(newMessage.value);
+        newMessage.value.content = '';
+        fetchMessages();
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+};
+const getUsername = (sendById: string): string => {
+    if (!list_coloc.value.length) return 'Unknown';
+    if (sendById === userStore.user.id) return 'me';
+    const user = list_coloc.value.find(coloc => coloc.id === sendById);
+    return user ? user.username : 'Unknown';
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.svg-icon {
+    width: 24px; /* Ajustez la largeur */
+    height: 24px; /* Ajustez la hauteur */
+    display: inline-block;
+    vertical-align: middle;
+}
+</style>
