326d741aec6c46462c43c0b6a9498d5597422c68
raymond_epitech <raymond-octave-pacome.vierge-thalamas@epitech.eu>
2025-08-22 17:32:51 +0200

    Wip Change css settings to avoid scaling issues on mobile.

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -27,77 +27,77 @@
 </template>
 
 <script setup>
-    const router = useRouter();
-    const redirect = (page) => {
-        router.push(page);
-    }
+const router = useRouter();
+const redirect = (page) => {
+    router.push(page);
+}
 </script>
 
 <style scoped>
-    .navbar {
-        position: fixed;
-        bottom: 0px;
-        width: 100%;
-        height: 8%;
-        background-color: var(--main-buttons-light);
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        justify-content: center;
-        align-items: center;
-        padding: 0%;
-    }
+.navbar {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 4.5rem; /* ~8% of a typical 56rem tall viewport */
+    background-color: var(--main-buttons-light);
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+}
 
-    .dark .navbar {
-        background-color: var(--main-buttons-dark);
-    }
+.dark .navbar {
+    background-color: var(--main-buttons-dark);
+}
 
-    .icon-subtext {
-        font-size: 65%;
-        margin-top: 1%;
-        font-weight: bold;
-        list-style: none;
-        text-align: center;
-        display: block;
-        color: var(--basic-grey);
-    }
+.icon-subtext {
+    font-size: 0.7rem; /* ~65% of base font size */
+    margin-top: 0.2rem;
+    font-weight: bold;
+    list-style: none;
+    text-align: center;
+    display: block;
+    color: var(--basic-grey);
+}
 
-    .dark .icon-subtext {
-        color: var(--light-grey);
-    }
+.dark .icon-subtext {
+    color: var(--light-grey);
+}
 
-    .button {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 100%;
-        padding-top: 7%;
-        margin-top: 5%;
-        text-decoration: none;
-    }
+.button {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    padding-top: 0.3rem; /* ~7% of 4.5rem */
+    margin-top: 0.2rem;  /* ~5% of 4.5rem */
+    text-decoration: none;
+}
 
-    .icon {
-        height: 60%;
-        filter: brightness(29%);
-    }
+.icon {
+    height: 2rem; /* ~60% of 4.5rem */
+    filter: brightness(29%);
+}
 
-    .dark .icon {
-        color: var(--page-text-dark);
-        filter: none;
-    }
+.dark .icon {
+    color: var(--page-text-dark);
+    filter: none;
+}
 
-    .wall-button {
-        max-width: 100%;
-        height: 35px;
-        display: flex;
-        justify-content: center;
-    }
+.wall-button {
+    max-width: 100%;
+    height: 3.5rem; /* ~78% of 4.5rem */
+    display: flex;
+    justify-content: center;
+}
 
-    .fridge {
-        display: flex;
-        max-width: 175%;
-        height: 400%;
-        margin-top: -40%;
-    }
+.fridge {
+    display: block;
+    max-width: 100%;
+    height: 10rem;
+    margin-top: -7rem;
+}
 </style>
