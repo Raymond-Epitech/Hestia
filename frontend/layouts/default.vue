9cef403b2bc6ce7a175b11fab4ac6e6e36e4158b
raymond_epitech <raymond-octave-pacome.vierge-thalamas@epitech.eu>
2025-08-24 21:07:57 +0200

    remove body container

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
 <template>
   <div>
-    <div class="body-container">
-      <slot />
-    </div>
     <footer>
       <Navbar />
     </footer>
@@ -36,15 +33,6 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-.body-container {
-  position: absolute;
-  /* border-top: 1px solid #f3f3f3;
-  top: 25pt;
-  bottom: 58px; */
-  left: 0;
-  right: 0;
-  overflow: auto;
-}
 
 .dark .body-container {
   border-top: 1px solid #333333;
