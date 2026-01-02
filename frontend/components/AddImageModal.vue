2bbc4cbe92060311a0af6d7a3866e8e3fc721933
Benjamin <benjamin.bourez@epitech.eu>
2026-01-02 11:11:37 +0100

    Update reminderType to 1 in AddImageModal component

--- frontend/components/AddImageModal.vue
+++ frontend/components/AddImageModal.vue
@@ -98,7 +98,7 @@ const resetPost = () => {
     coordX: 0,
     coordY: 0,
     coordZ: 0,
-    reminderType: 0,
+    reminderType: 1,
     content: '',
     color: '',
     image: new File([], 'test.jpg'),
